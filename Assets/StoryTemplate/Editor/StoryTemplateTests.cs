40e98fae9fd91a82528c6ef974772eab66ab47d5
vedran-brnjetic <Vedran.Brnjetic@metropolia.fi>
2018-01-30 19:24:57 +0200

    StoryCanvas created

---
+++ Assets/StoryTemplate/Editor/StoryTemplateTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+
+public class StoryTemplateTests {
+
+    private static Canvas GetCanvas(string cnvName)
+    {
+        return GameObject.Find(cnvName).GetComponent<Canvas>();
+    }
+
+    [Test]
+    public void Is_The_StoryCanvas_Present()
+    {
+        var cnvName = "StoryCanvas";
+        var canvas = GetCanvas(cnvName);
+
+        Assert.IsNotNull(canvas);
+    }
+    
+}
