01bae82d140ce1d52a54c1d3a75548a698322891
vedran-brnjetic <Vedran.Brnjetic@metropolia.fi>
2018-01-31 08:26:12 +0200

    Added MemorySequence Canvas

--- Assets/StoryTemplate/Editor/StoryTemplateTests.cs
+++ Assets/StoryTemplate/Editor/StoryTemplateTests.cs
@@ -2,70 +2,81 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-
-public class StoryTemplateTests {
+namespace Assets.StoryTemplate.Editor
+{
+    public class StoryTemplateTests {
     
-    public class Canvases
-    {
-
-        private static Canvas GetCanvas(string cnvName)
+        public class Canvases
         {
-            return GameObject.Find(cnvName).GetComponent<Canvas>();
+
+            private static Canvas GetCanvas(string cnvName)
+            {
+                return GameObject.Find(cnvName).GetComponent<Canvas>();
+            }
+
+            [Test]
+            public void Is_The_StoryCanvas_Present()
+            {
+                const string cnvName = "StoryCanvas";
+                var canvas = GetCanvas(cnvName);
+
+                Assert.IsNotNull(canvas);
+            }
+
+            [Test]
+            public void Is_The_StoryIntroCanvas_Present()
+            {
+                const string cnvName = "StoryIntroCanvas";
+                var canvas = GetCanvas(cnvName);
+
+                Assert.IsNotNull(canvas);
+            }
+
+            [Test]
+            public void Is_The_StoryCharacterDialogCanvas_Present()
+            {
+                const string cnvName = "StoryCharacterDialogCanvas";
+                var canvas = GetCanvas(cnvName);
+
+                Assert.IsNotNull(canvas);
+            }
+
+            [Test]
+            public void Is_The_StoryObjectDialogCanvas_Present()
+            {
+                const string cnvName = "StoryObjectDialogCanvas";
+                var canvas = GetCanvas(cnvName);
+
+                Assert.IsNotNull(canvas);
+            }
+            [Test]
+            public void Is_The_StoryMemorySequenceCanvas_Present()
+            {
+                const string cnvName = "StoryMemorySequenceCanvas";
+                var canvas = GetCanvas(cnvName);
+
+                Assert.IsNotNull(canvas);
+            }
+
+
         }
 
-        [Test]
-        public void Is_The_StoryCanvas_Present()
+        public class Buttons
         {
-            var cnvName = "StoryCanvas";
-            var canvas = GetCanvas(cnvName);
+            private static Button GetButton(string btnName)
+            {
+                return GameObject.Find(btnName).GetComponent<Button>();
+            }
 
-            Assert.IsNotNull(canvas);
-        }
+            [Test]
+            public void Exit_Button_Present()
+            {
+                var btnName = "btnExit";
+                var testButton = GetButton(btnName);
 
-        [Test]
-        public void Is_The_StoryIntroCanvas_Present()
-        {
-            var cnvName = "StoryIntroCanvas";
-            var canvas = GetCanvas(cnvName);
-
-            Assert.IsNotNull(canvas);
-        }
-
-        [Test]
-        public void Is_The_StoryCharacterDialogCanvas_Present()
-        {
-            var cnvName = "StoryCharacterDialogCanvas";
-            var canvas = GetCanvas(cnvName);
-
-            Assert.IsNotNull(canvas);
-        }
-
-        [Test]
-        public void Is_The_StoryObjectDialogCanvas_Present()
-        {
-            var cnvName = "StoryObjectDialogCanvas";
-            var canvas = GetCanvas(cnvName);
-
-            Assert.IsNotNull(canvas);
-        }
-
-
-    }
-
-    public class Buttons
-    {
-        private static Button GetButton(string btnName)
-        {
-            return GameObject.Find(btnName).GetComponent<Button>();
-        }
-
-        [Test]
-        public void Exit_Button_Present()
-        {
-            var btnName = "btnExit";
-            var testButton = GetButton(btnName);
-
-            Assert.IsNotNull(testButton);
+                Assert.IsNotNull(testButton);
+            
+            }
         }
     }
 }
