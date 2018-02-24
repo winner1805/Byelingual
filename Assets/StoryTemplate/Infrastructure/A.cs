be0ed12942f0e28084204c02da2f436b9701d2b5
vedran-brnjetic <Vedran.Brnjetic@metropolia.fi>
2018-02-24 23:15:41 +0200

    Commented on A and An semantic classes

--- Assets/StoryTemplate/Infrastructure/A.cs
+++ Assets/StoryTemplate/Infrastructure/A.cs
@@ -1,6 +1,11 @@
 ﻿namespace Assets.StoryTemplate.Infrastructure
 {
     public static class A
+    /*
+     * This class is created solely for semantic purposes to write a more readable code
+     * So when you need a new Canvas in game you just write
+     * var canvas = A.Canvas();
+     */
     {
         public static CanvasBuilder Canvas()
         {
--- Assets/StoryTemplate/Infrastructure/An.cs
+++ Assets/StoryTemplate/Infrastructure/An.cs
@@ -2,6 +2,11 @@
 {
     public static class An
     {
+        /*
+         * This class is created solely for semantic purposes to write a more readable code
+         * So when you need a new Image in game you just write
+         * var image = An.Image();
+         */
         public static ImageBuilder Image()
         {
             return new ImageBuilder();
